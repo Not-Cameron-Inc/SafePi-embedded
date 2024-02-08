8269c03255f8197b585e784ae0cb110ec69d79dd
Elliot Shabram <elliotshabram@Elliots-MacBook-Air.local>
2024-02-07 21:36:18 -0800

    small edits

--- connect.py
+++ connect.py
@@ -6,6 +6,8 @@ async def find_device(char_uuid, data_bytes):
     if device is not None:
         print(f'Found SafePi! {device}')
         async with BleakClient(device.address) as client:
+            print(client.is_connected)
+            input("Pause (press enter)")
             try:
                 await client.write_gatt_char(char_uuid, data=data_bytes)
                 print("Write successful")
@@ -13,7 +15,7 @@ async def find_device(char_uuid, data_bytes):
                 print(f"Error while writing: {e}")
 
 if __name__ == "__main__":
-    data = "We have data transfer."
-    data_bytes = data.encode('utf-8')
+    data = b'0xf'
+    # data_bytes = data.encode('utf-8')
     char_uuid = '51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B' 
-    asyncio.run(find_device(char_uuid, data_bytes))
+    asyncio.run(find_device(char_uuid, data))
