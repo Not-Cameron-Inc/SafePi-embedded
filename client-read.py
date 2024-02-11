94baa6707577e23f3bc35cb18dc6326dabd4aa49
Elliot Shabram <elliotshabram@Elliots-MacBook-Air.local>
2024-02-10 22:08:11 -0800

    nothing

--- client-read.py
+++ client-read.py
@@ -2,14 +2,13 @@ import asyncio
 from bleak import BleakScanner, BleakClient
 
 async def run():
-    device = await BleakScanner.find_device_by_name(name='SafePiServer', timeout=15.0)
+    device = await BleakScanner.find_device_by_name(name='SafePi', timeout=15.0)
     if device:
+        # No need to call client.connect() explicitly when using async with
         async with BleakClient(device.address) as client:
-            await client.connect()
             value = await client.read_gatt_char("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
             print("Read value:", value.decode('utf-8'))
     else:
         print("Device not found")
 
 asyncio.run(run())
-
