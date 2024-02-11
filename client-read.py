616d1a539b31221b082b2e6138c1adf9806bed41
Elliot Shabram <elliotshabram@Elliots-MacBook-Air.local>
2024-02-10 22:14:01 -0800

    nothing

--- client-read.py
+++ client-read.py
@@ -1,13 +1,22 @@
 import asyncio
-from bleak import BleakScanner, BleakClient
+from bleak import BleakClient, BleakScanner
+
+CHAR_UUID = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+DATA_TO_WRITE = b'Hello from client'
 
 async def run():
-    device = await BleakScanner.find_device_by_name(name='SafePi', timeout=15.0)
+    device = await BleakScanner.find_device_by_name(name='SafePiServer', timeout=15.0)
     if device:
-        # No need to call client.connect() explicitly when using async with
         async with BleakClient(device.address) as client:
-            value = await client.read_gatt_char("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
-            print("Read value:", value.decode('utf-8'))
+            # Writing to the characteristic
+            print(f"Writing to characteristic {CHAR_UUID}: {DATA_TO_WRITE}")
+            await client.write_gatt_char(CHAR_UUID, DATA_TO_WRITE)
+            print("Write successful")
+
+            # Reading back the written value
+            value = await client.read_gatt_char(CHAR_UUID)
+            print(f"Read from characteristic {CHAR_UUID}: {value.decode('utf-8')}")
+
     else:
         print("Device not found")
 
