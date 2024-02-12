1bfa015ace1020d88699f8e4f8fa989c73415bc8
Elliot Shabram <elliotshabram@Elliots-MacBook-Air.local>
2024-02-11 21:07:30 -0800

    nothing

--- server-noencryption.py
+++ server-noencryption.py
@@ -20,7 +20,7 @@ def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray
 def write_request(characteristic: BlessGATTCharacteristic, value: Any, **kwargs):
     logger.debug(f"Write request to {characteristic.uuid}: {value}")
     # Here you can handle incoming write requests, if needed
-    logger.debug(f'Value Written: {value}')
+    logger.debug(f'Value Written: {value.decode('utf-8')}')
     
 
 async def run(loop):
