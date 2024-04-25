52241f7e3e4402e868b5387f2aee72ef47927ddd
Elliot Shabram <elliotshabram@gmail.com>
2024-04-25 00:28:41 -0700

    fixed?

--- safepi.py
+++ safepi.py
@@ -3,15 +3,12 @@
 import asyncio
 import time
 from utils import *
-from server import run\
+from server import run_server
 
 # Function to start the BLE server
 async def start_server():
     loop = asyncio.get_event_loop()
     await run(loop)
-
-
-
 
 if __name__ == "__main__":
     # start a thread for device function
@@ -19,7 +16,8 @@ if __name__ == "__main__":
     server_thread.start()
 
     # turn on the BLE server
+    loop = asyncio.get_event_loop()
     try:
-        asyncio.run(start_server())
+        loop.run_until_complete(run_server(loop))
     except KeyboardInterrupt as e:
         print(e)
--- server.py
+++ server.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
-
 import logging
 import asyncio
-
 from utils import *
 from typing import Any, Dict
 from bless import BlessServer, BlessGATTCharacteristic, GATTCharacteristicProperties, GATTAttributePermissions
@@ -10,11 +7,9 @@ from bless import BlessServer, BlessGATTCharacteristic, GATTCharacteristicProper
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(name=__name__)
 
-
 def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
     # This is where we define what data is returned when the characteristic is read
     # For example, return a simple string converted to bytes"
-
     message = CONNECTION_STATUS
     encrypted_message = encrypt(message, '', '')
 
@@ -23,17 +18,15 @@ def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray
     logger.debug(f"Reading {characteristic.uuid}: {encrypted_message}")
     return bytearray(encrypted_message)
 
-
 def write_request(characteristic: BlessGATTCharacteristic, value: Any, **kwargs):
     logger.debug(f"Write request to {characteristic.uuid}: {value}")
     # log the decrypted write
     message = decrypt(value, AES_KEY, IV)
     logger.debug(f'Decrypted: {message}')
-    # send the data to be handled. 
+    # send the data to be handled.
     handle_write(message)
-    
-    
-async def run(loop):
+
+async def run_server(loop):
     #define a GATT server with one service and two characteristics. One for reading, and the other for writing.
     gatt: Dict = {
         "A07498CA-AD5B-474E-940D-16F1FBE7E8CD": {
@@ -62,8 +55,3 @@ async def run(loop):
     # Keep the server running
     while True:
         await asyncio.sleep(1)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run(loop))
-
-
